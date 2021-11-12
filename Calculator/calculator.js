53406c3a5c76f551c95f7782399d160995a6ca92
Arasjie <martijn.valkenborgh@student.pxl.be>
2021-11-12 20:09:04 +0100

    express update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -19,6 +19,27 @@ app.post("/", function(req, res) {
   res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmiCalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req, res) {
+
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
+  var bmi = weight / (height * height);
+  var interpretation;
+
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    } else if (bmi > 18.5 && bmi < 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+  res.send(interpretation);
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
